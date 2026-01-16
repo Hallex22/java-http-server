afcf278f815c211af8f709cbc1745dfdc1bdeba2
Alexandru-Marian Pirvu <pirvualexandru22@stud.ase.ro>
2026-01-16 16:48:12 +0200

    Server logging client request and seding placeholder 200 OK always

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,17 +13,52 @@ public class HttpServer {
   }
 
   public void run() {
-    System.out.println("Server is running on port " + port);
     try {
       ServerSocket server = new ServerSocket(port);
+      System.out.println("🚀 Server is running on port " + port);
       while (true) {
         Socket client = server.accept();
-        System.out.println("Client accepted!");
+
+        String rawRequest = readAllFromClient(client);
+
+        handleRequest(rawRequest);
+
+        PrintWriter out = new PrintWriter(client.getOutputStream());
+        out.write("HTTP/1.1 200 OK\r\n");
+        out.write("Content-Type: text/plain\r\n");
+        out.write("Content-Length: 2\r\n");
+        out.write("\r\n");
+        out.write("OK");
+        out.flush();
+
         client.close();
       }
     } catch (Exception e) {
-      System.out.println("Port occupied, try another one");
+      System.out.println("❌ Port occupied, try another one");
     }
 
   }
+
+  private String readAllFromClient(Socket client) {
+    StringBuilder rawRequest = new StringBuilder();
+    try {
+      BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+      String line;
+      while ((line = br.readLine()) != null) {
+        if (line.isEmpty()) {
+          break;
+        }
+        rawRequest.append(line).append("\r\n");
+      }
+    } catch (IOException e) {
+      // TODO: handle exception
+    }
+    return rawRequest.toString();
+  }
+
+  private void handleRequest(String rawRequest) {
+    // TODO: handle the user request
+    System.out.println(rawRequest);
+  }
+
 }
