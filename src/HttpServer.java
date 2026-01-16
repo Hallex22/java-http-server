554f6c0fe9d2e1a5f020687d8904d91421a503e2
Alexandru-Marian Pirvu <pirvualexandru22@stud.ase.ro>
2026-01-16 11:24:06 +0200

    ServerSocket added and client Socket, with debug message

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,11 +1,25 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
-    private int PORT;
+  private int port;
 
-    public HttpServer(int port) {
-        this.PORT = port;
+  public HttpServer(int port) {
+    this.port = port;
+  }
+
+  public void run() {
+    System.out.println("Server is running on port " + port);
+    try {
+      ServerSocket server = new ServerSocket(port);
+      while (true) {
+        Socket client = server.accept();
+        System.out.println("Client accepted!");
+        client.close();
+      }
+    } catch (Exception e) {
+      System.out.println("Port occupied, try another one");
     }
 
-    public void run() {
-        System.out.println("Server is running on port " + PORT);
-    }
+  }
 }
